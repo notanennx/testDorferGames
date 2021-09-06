17c460f09f70612236e2f7d31ba9826839d1734d
notanennx <41900703+notanennx@users.noreply.github.com>
2021-09-06 17:25:35 +0400

    Initial commit

---
+++ Assets/Script/MaterialColor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialColor : MonoBehaviour
+{
+    // Init
+    private Material material;
+
+    // Start
+    void Start()
+    {   
+        //material = GetComponent<Renderer>().material;
+
+
+    }
+
+    // Update
+    void Update()
+    {
+        
+    }
+}
