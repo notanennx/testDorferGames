cc5780f929f09c88d33d51cf9b6e4f0740bf0d8b
notanennx <41900703+notanennx@users.noreply.github.com>
2021-09-06 18:12:38 +0400

    added dummy clusters with materials

---
+++ Assets/Script/DummySpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummySpawner : MonoBehaviour
+{
+    // Init
+    [SerializeField] private int minAmount = 3;
+    [SerializeField] private int maxAmount = 5;
+    [SerializeField] private GameObject dummyObject;
+
+    // Start
+    void Start()
+    {
+        Material[] dummyMaterials = Resources.LoadAll<Material>("Materials/Colors");
+        SpawnCluster(dummyMaterials[Random.Range(0, dummyMaterials.Length)]);
+    }
+
+    // Spawn Dummy
+    void SpawnDummy(Material dummyMaterial)
+    {
+        int attempts = 16;
+        while (attempts > 0)
+        {
+            Vector3 newPos = transform.position + (new Vector3(Random.Range(-1f, 1f), 0.75f, Random.Range(-1f, 1f)));
+            if (!Physics.CheckCapsule(newPos, newPos, 0.25f))
+            {
+                GameObject newDummy = Instantiate(dummyObject, newPos, transform.rotation, transform);
+                    newDummy.GetComponent<MeshRenderer>().material = dummyMaterial;
+
+                break;
+            }
+            else
+            {
+                attempts -= 1;
+            }
+        }
+    }
+
+    // Spawn Cluster
+    void SpawnCluster(Material dummyMaterial)
+    {
+        int amount = Random.Range(minAmount, maxAmount);
+        for (int i = 0;  i < amount; i++) SpawnDummy(dummyMaterial);
+    }
+}
