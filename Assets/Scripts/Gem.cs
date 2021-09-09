aae3b32332d8a2a5341a46aa8779b0afe880dac7
notanennx <41900703+notanennx@users.noreply.github.com>
2021-09-09 23:09:17 +0400

    added gems

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour, ICollectable
+{
+    // Start
+    private float speed;
+    [SerializeField] private Transform model;
+    private void Start()
+    {
+        speed = Random.Range(16f, 24f);
+    }
+
+    // Update
+    private void Update()
+    {
+        model.localPosition = new Vector3(0f, 0.25f * Mathf.Sin(4 * Time.time), 0f);
+        transform.Rotate(0, (speed * Time.deltaTime), 0, Space.Self);
+    }
+
+    // OnCollected
+    void ICollectable.OnCollected()
+    {
+        Score.i.Gems += 1;
+        Destroy(gameObject);
+    }
+}
