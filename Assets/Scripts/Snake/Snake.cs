dd75689e91a64d382ae5681d074ac3cc688b5645
notanennx <41900703+notanennx@users.noreply.github.com>
2021-09-17 12:11:21 +0400

    wow, tail is actually working now!

--- Assets/Scripts/Snake/Snake.cs
+++ Assets/Scripts/Snake/Snake.cs
@@ -8,9 +8,21 @@ public class Snake : MonoBehaviour
     [SerializeField] private float forwardSpeed = 16f;
     [SerializeField] [Range(0.01f, 1f)] private float sideLerp = 0.16f;
 
+    // Snake
+    public int Length = 3;
+    public int CollideLength = 2;
+
     // Awake
     public static Snake i;
     private void Awake() => i = this;
+
+    // Start
+    private Tail tail;
+    private void Start()
+    {
+        tail = GetComponent<Tail>();
+        for (int i = 0; i < Length; i++) tail.AddPart();
+    }
 
     // On Trigger Enter
     private void OnTriggerEnter(Collider other)
@@ -40,26 +52,4 @@ public class Snake : MonoBehaviour
         // Forward
         snakeHolder.Translate((-Vector3.right * (forwardSpeed * Time.deltaTime)));
     }
-
-    /*
-    // Update
-    private void Update()
-    {
-        // Get
-        //Vector3 mousePos = Input.mousePosition;
-        //Vector3 snakePos = Camera.main.WorldToScreenPoint(Snake.transform.position);
-
-        // Moving
-        float posDiff = (mousePos.x - snakePos.x);
-        Vector3 curPos = Snake.transform.localPosition;
-        Vector3 newPos = (curPos + new Vector3(0f, 0f, posDiff));
-        Snake.transform.localPosition = Vector3.Lerp(curPos, newPos, (sideLerp * Time.deltaTime));
-
-        // Forward
-        transform.Translate((-Vector3.right * (forwardSpeed * Time.deltaTime)));
-
-        // Clamping
-    //Snake.transform.localPosition = new Vector3(0f, 0.5f, Mathf.Clamp(Snake.transform.localPosition.z, -roadWidth, roadWidth));
-    }
-    */
 }
---
+++ Assets/Scripts/Snake/Tail.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tail : MonoBehaviour
+{
+    [SerializeField] private float partSize = 1f;
+    [SerializeField] private Transform connector;
+    [SerializeField] private Transform partsHolder;
+    [SerializeField] private GameObject partObject;
+
+    private List<Vector3> positions = new List<Vector3>();
+    private List<Transform> parts = new List<Transform>();
+
+    void Awake()
+    {
+        positions.Add(connector.position);
+    }
+
+    // Update
+    void Update()
+    {
+        float distance = (connector.position - positions[0]).magnitude;
+
+        // Change positions
+        if (distance > partSize)
+        {
+            Vector3 direction = (connector.position - positions[0]).normalized;
+
+            positions.Insert(0, positions[0] + direction * partSize);
+            positions.RemoveAt(positions.Count - 1);
+
+            distance -= partSize;
+        }
+
+        // Move our snake parts
+        for (int i = 0; i < parts.Count; i++)
+        {
+            parts[i].position = Vector3.Lerp(positions[i + 1], positions[i], (distance / partSize));
+        }
+    }
+
+    // Adds part
+    public void AddPart()
+    {
+        GameObject newPart = Instantiate(partObject, positions[positions.Count - 1], connector.rotation, transform);
+            Transform newTransform = newPart.GetComponent<Transform>();
+                newTransform.SetParent(partsHolder);
+
+            parts.Add(newTransform);
+            positions.Add(newTransform.position);
+    }
+
+    // Removes part
+    public void RemoveCircle()
+    {
+        Destroy(parts[0].gameObject);
+
+        parts.RemoveAt(0);
+        positions.RemoveAt(1);
+    }
+}
